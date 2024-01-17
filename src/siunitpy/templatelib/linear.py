c78af5b036b6c4c26dd78e3d08cbc36f53bca4ac
dait <daitthu@qq.com>
2024-01-17 15:55:08 +0800

    modify linear (to be deprecated)

--- src/siunitpy/templatelib/linear.py
+++ src/siunitpy/templatelib/linear.py
@@ -2,8 +2,9 @@ import numbers
 from abc import ABC, abstractmethod
 from decimal import Decimal
 from fractions import Fraction
+from typing import NoReturn, TypeVar
 
-__all__ = ['Linear']
+__all__ = ['Linear', 'LinearType']
 
 
 class Linear(ABC):
@@ -11,29 +12,49 @@ class Linear(ABC):
     '''
     __slots__ = ()
     @abstractmethod
-    def __eq__(self, other): raise NotImplementedError
+    def __abs__(self) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __ne__(self, other): raise NotImplementedError
+    def __eq__(self, other) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __gt__(self, other): raise NotImplementedError
+    def __ne__(self, other) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __lt__(self, other): raise NotImplementedError
+    def __gt__(self, other) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __ge__(self, other): raise NotImplementedError
+    def __lt__(self, other) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __le__(self, other): raise NotImplementedError
-    # @abstractmethod
-    # def __abs__(self): raise NotImplementedError
+    def __ge__(self, other) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __add__(self, othre): raise NotImplementedError
+    def __le__(self, other) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __sub__(self, other): raise NotImplementedError
+    def __add__(self, other) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __mul__(self, other): raise NotImplementedError
+    def __sub__(self, other) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __truediv__(self, other): raise NotImplementedError
+    def __mul__(self, other) -> NoReturn: raise NotImplementedError
     @abstractmethod
-    def __pow__(self, other): raise NotImplementedError
+    def __truediv__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __pow__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __iadd__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __isub__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __imul__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __itruediv__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __ipow__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __radd__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __rsub__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __rmul__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __rtruediv__(self, other) -> NoReturn: raise NotImplementedError
+    @abstractmethod
+    def __rpow__(self, other) -> NoReturn: raise NotImplementedError
 
 
 Linear.register(int)
@@ -45,3 +66,6 @@ try:
     Linear.register(np.ndarray)
 except ImportError:
     pass
+
+LinearType = TypeVar('LinearType', int, float, Fraction,
+                     Decimal, np.ndarray, Linear)

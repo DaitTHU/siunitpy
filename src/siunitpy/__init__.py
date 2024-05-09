2f6ee531a3d88b1fac25a925fa45a6f60695b324
dait <daitthu@qq.com>
2024-05-09 15:19:30 +0800

    decouple of unit and quantity

--- src/siunitpy/__init__.py
+++ src/siunitpy/__init__.py
@@ -4,6 +4,6 @@ from . import SI, utilcollections
 from .constant import Constant
 from .dimension import Dimension
 from .dimensionconst import DimensionConst
-from .quantity import Quantity, Unit
-from .unitconst import UnitConst
+from .quantity import Quantity
+from .unit import Unit
 from .variable import Variable

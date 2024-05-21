d01f3c849257ccb909dbce843c3d71c5d83c32e9
dait <daitthu@qq.com>
2024-05-21 21:38:18 +0800

    update SI quantity

--- src/siunitpy/SI/SIquantity.py
+++ src/siunitpy/SI/SIquantity.py
@@ -1,5 +1,5 @@
 from ..constant import Constant, constant
-from ..value_archive import _PI, _WEIN_ZERO, _GRAVITY, _ATM
+from ..value_archive import PI, WEIN_F_ZERO, WEIN_ZERO, GRAVITY
 from ..utilcollections.constclass import ConstClass
 from .SIunit import si
 
@@ -15,120 +15,142 @@ class SI(ConstClass):
     use them directly.
     '''
 
-    ### exact constants defined by SI
+    # exact constants defined by SI
 
-    # hyperfine transition frequency of Cs-133
     nu133Cs = Constant(9_192_631_770, 'Hz')
-    # speed of light in vacuum
+    '''hyperfine transition frequency of Cs-133'''
     c = Constant(299_792_458, 'm/s')
-    # Planck constant
+    '''speed of light in vacuum'''
     h = Constant(6.626_070_15e-34, 'J.s')
-    # atomic unit of charge
+    '''Planck constant'''
     e = Constant(1.602_176_634e-19, 'C')
-    # Boltzmann constant
+    '''elementary charge'''
     kB = Constant(1.380_649e-23, 'J/K')
-    # Avogadro constant
+    '''Boltzmann constant'''
     NA = Constant(6.022_140_76e23, 'mol-1')
-    # luminous efficacy
+    '''Avogadro constant'''
     Kcd = Constant(683, 'lm/W')
+    '''luminous efficacy'''
 
-    ### exact constants derived from SI
+    # universal
 
-    # molar gas constant
-    R = constant(kB * NA)
-    # Faraday constant
-    F = constant(NA * e)
-    # molar Planck constant
-    # NAh = constant(NA * h)
-    # reduced Planck constant
-    hbar = constant(h / (2 * _PI))
-    # Stefan-Boltzmann constant
-    sigma = constant((_PI**2/60 * kB**4 / (hbar**3 * c**2)).ito('W/m2.K4'))
-    # first radiation constant for spectral radiance
-    c1L = constant((2 * h * c**2).ito('W.m2/sr'))
-    # first radiation constant
-    c1 = constant(c1L * _PI * si.sr)
-    # second radiation constant
-    c2 = constant(h * c / kB)
-    # Wien wavelength displacement law constant
-    b = constant(c2 / _WEIN_ZERO)
-    # conductance quantum
-    G0 = constant((2 * e**2 / h).simplify_unit(inplace=True))
-    # Josephson constant
-    KJ = constant((2 * e / h).ito('Hz/V'))
-    # magnetic flux quantum
-    Phi0 = constant((1 / KJ).simplify_unit(inplace=True))
-    # von Klitzing constant
-    RK = constant((h / e**2).simplify_unit(inplace=True))
+    hbar = constant(h / (2 * PI))
+    '''reduced Planck constant'''
+    G = Constant(6.67430e-11, 'm3/kg.s2', relative_uncertainty=2.2e-5)
+    '''Newtonian constant of gravitation'''
+    mP = constant((hbar * c / G).nthroot(2), simplify=True)
+    '''Planck mass'''
+    TP = constant(mP * c**2 / kB, simplify=True)
+    '''Planck temperature'''
+    lP = constant(hbar / (mP * c), simplify=True)
+    '''Planck length'''
+    tP = constant(lP / c)
+    '''Planck time'''
 
-    ### other defined exact constants
+    # electromagnetic constants
 
-    # standard acceleration of gravity
-    g = Constant(_GRAVITY, 'm/s2')
-    # standard temperature
-    T0 = Constant(273.15, 'K')
-    # standard pressure
-    p0 = Constant(_ATM, 'Pa')
-    # molar volume of ideal gas
-    Vm = constant((R * T0 / p0).ito('m3/mol'))
+    mu0 = Constant(1.256_637_062_12e-6, 'H/m', relative_uncertainty=1.5e-10)
+    '''vacuum magnetic permeability'''
+    epsilon0 = constant(1 / (mu0 * c**2), 'F/m')
+    '''vacuum electric permittivity'''
+    Z0 = constant(mu0 * c, simplify=True)
+    '''characteristic impedance of vacuum'''
+    # ke = constant(1 / (4 * _PI * epsilon0))
+    # '''Coulomb constant'''
+    # km = constant(mu0 / (4 * _PI))
+    # '''magnetic constant'''
+    KJ = constant(2 * e / h, 'Hz/V')
+    '''Josephson constant'''
+    Phi0 = constant(1 / KJ, simplify=True)
+    '''magnetic flux quantum'''
+    G0 = constant(2 * e**2 / h, simplify=True)
+    '''conductance quantum'''
+    RK = constant(h / e**2, simplify=True)
+    '''von Klitzing constant'''
 
-    ### constants with uncertainty
+    # atomic and nuclear
 
-    # atomic mass constant
-    mu = Constant(1.660_539_066_60e-27, 'kg',
-                  0.000_000_000_50e-27)
-    # molar mass constant
-    Mu = (mu * NA).ito('g/mol')
-    # molar mass of C-12
-    M12C = constant(12 * Mu)
-    # Newtonian constant of gravitation
-    G = Constant(6.67430e-11, 'm3/kg.s2',
-                 0.000_15e-11)
-    # vacuum electric permittivity
-    epsilon0 = Constant(8.854_187_8128e-12, 'F/m',
-                        0.000_000_0013e-12)
-    # vacuum magnetic permeability
-    mu0 = Constant(1.256_637_062_12e-6, 'H/m',
-                   0.000_000_000_19e-6)
-    # Coulomb constant
-    ke = constant(1 / (4 * _PI * epsilon0))
-    # magnetic constant
-    km = constant(mu0 / (4 * _PI))
-    # characteristic impedance of vacuum
-    Z0 = constant((mu0 / epsilon0).simplify_unit(inplace=True).nthroot(2))
-    # fine-structure constant
-    alpha = constant((e**2 / (2 * epsilon0 * h * c)).simplify_unit(inplace=True))
-    # inverse fine-structure constant
+    me = Constant(9.109_383_7015e-31, 'kg', relative_uncertainty=3.0e-10)
+    '''electron mass'''
+    mmu = Constant(1.883_531_627e-28, 'kg', relative_uncertainty=2.2e-8)
+    '''muon mass'''
+    mtau = Constant(3.167_54e-27, 'kg', relative_uncertainty=6.8e-5)
+    '''tau mass'''
+    mp = Constant(1.672_621_923_69e-27, 'kg', relative_uncertainty=3.1e-10)
+    '''proton mass'''
+    mn = Constant(1.674_927_498_04e-27, 'kg', relative_uncertainty=5.7e-10)
+    '''neutron mass'''
+    md = Constant(3.343_583_7724e-27, 'kg', relative_uncertainty=3.0e-10)
+    '''deuteron mass'''
+    mt = Constant(5.007_356_7446e-27, 'kg', relative_uncertainty=3.0e-10)
+    '''triton mass'''
+    mh = Constant(5.006_412_7796e-27, 'kg', relative_uncertainty=3.0e-10)
+    '''helion mass'''
+    malpha = Constant(6.644_657_3357e-27, 'kg', relative_uncertainty=3.0e-10)
+    '''alpha partcle mass'''
+
+    alpha = constant(e**2 / (2 * epsilon0 * h * c), simplify=True)
+    '''fine-structure constant'''
     alphainv = constant(1 / alpha)
-    # particle mass
-    me = Constant(9.109_383_7015e-31, 'kg',
-                  0.000_000_0028e-31)
-    mp = Constant(1.672_621_923_69e-27, 'kg',
-                  0.000_000_000_51e-27)
-    mn = Constant(1.674_927_498_04e-27, 'kg',
-                  0.000_000_000_95e-27)
-    malpha = Constant(6.644_657_3357e-27, 'kg',
-                      0.000_000_0020e-27)
-    mmu = Constant(1.883_531_627e-28, 'kg',
-                   0.000_000_042e-28)
-    mtau = Constant(3.167_54e-27, 'kg',
-                    0.000_21e-27)
-    # electron charge to mass quotient
-    e_me = constant(e / me)
-    # Compton wavelength
-    lambdaC = constant((h / (me * c)).simplify_unit(inplace=True))
-    # classical electron radius
-    re = constant(alpha * lambdaC / (2 * _PI))
-    # Thomson cross section
-    sigmae = constant(8 * _PI / 3 * re**2)
-    # Bohr radius
-    a0 = constant(re / alpha**2)
-    # Rydberg constant = alpha**2 / 2 * lambdaC
-    Rinf = Constant(10_973_731.568_160, 'm-1',
-                    0.000_021)
-    # Hartree energy = alpha**2 * me * c**2
+    '''inverse fine-structure constant'''
+    a0 = constant(hbar / (alpha * me * c), relative_uncertainty=1.5e-10)
+    '''Bohr radius'''
+    lambdaC = constant(h / (me * c), simplify=True)
+    '''Compton wavelength'''
+    Rinf = constant(alpha**2 / (2 * lambdaC), relative_uncertainty=1.9e-12)
+    '''Rydberg constant'''
     Eh = constant(2 * h * c * Rinf)
-    # Bohr magneton = hbar / alpha * me * c
-    muB = constant((e_me * hbar / 2).ito('J/T'))
-    # nuclear magneton
-    muN = constant((e * hbar / (2 * mp)).ito('J/T'))
+    '''Hartree energy'''  # = alpha**2 * me * c**2
+    re = constant(alpha**2 * a0)
+    '''classical electron radius'''
+    sigmae = constant(8 * PI / 3 * re**2)
+    '''Thomson cross section'''
+    muB = constant(e * hbar / (2 * me), 'J/T')
+    '''Bohr magneton'''
+    muN = constant(e * hbar / (2 * mp), 'J/T')
+    '''nuclear magneton'''
+    mue = Constant(-9.284_764_7043e-24, 'J/T', relative_uncertainty=3.0e-10)
+    '''electron magnetic moment'''
+    ge = constant(2 * mue / muB, relative_uncertainty=1.7e-13)
+    '''electron g-factor'''
+
+    # physicochemical
+
+    mu = Constant(1.660_539_066_60e-27, 'kg', relative_uncertainty=3.0e-10)
+    '''atomic mass constant'''
+    Mu = constant(mu * NA)
+    '''molar mass constant'''
+    # M12C = constant(12 * Mu)
+    # '''molar mass of C-12'''
+    # NAh = constant(NA * h)
+    # '''molar Planck constant'''
+    R = constant(kB * NA)
+    '''molar gas constant'''
+    F = constant(NA * e)
+    '''Faraday constant'''
+
+    sigma = constant(PI**2/60 * kB**4 / (hbar**3 * c**2), 'W/m2.K4')
+    '''Stefan-Boltzmann constant'''
+    c1L = constant((2 * h * c**2).ito('W.m2/sr'))
+    '''first radiation constant for spectral radiance'''
+    c1 = constant(c1L * PI * si.sr)
+    '''first radiation constant'''
+    c2 = constant(h * c / kB)
+    '''second radiation constant'''
+    b = constant(c2 / WEIN_ZERO)
+    '''Wien wavelength displacement law constant'''
+    b_ = constant(WEIN_F_ZERO * c / c2, 'Hz/K')
+    '''Wien frequency displacement law constant'''
+
+    # other defined exact constants
+
+    g = Constant(GRAVITY, 'm/s2')
+    '''standard acceleration of gravity'''
+    T0 = Constant(273.15, 'K')
+    '''standard temperature'''
+    p0 = Constant(100_000, 'Pa')
+    '''standard pressure'''
+    Vm = constant((R * T0 / p0).ito('m3/mol'))
+    '''molar volume of ideal gas (273.5 K, 100 kPa)'''
+    n0 = constant(NA / Vm)
+    '''Loschmidt constant (273.5 K, 100 kPa)'''
